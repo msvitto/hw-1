14b5f5440b6ca9f6c44f1b558076b684828748a1
marina svitto <marinamironchik04@gmail.com>
2024-02-04 12:45:20 +0200

    change section purchase_option, add lines, align block purchase_checkbox

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+
+
 const checkBoxes = document.querySelectorAll('.cursor-point');
-checkBoxes.forEach(checkBox => {
-    checkBox.addEventListener('click', function () {
+checkBoxes.forEach(checkBox=>{
+    checkBox.addEventListener('click', function (){
         this.classList.toggle('active');
         console.log(this);
     })
-})
+});
+
+const checkBoxesText = document.querySelectorAll('.cursor-point-text');
+checkBoxesText.forEach(checkBoxText =>{
+    checkBoxText.addEventListener('click', function (){
+        this.classList.toggle('active');
+        console.log(this);
+    })
+});
+
+
