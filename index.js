b9342ca353c7bd0fda30232fc50f4a6d0318feb2
marina svitto <marinamironchik04@gmail.com>
2024-02-05 10:04:33 +0200

    change js. Add eventListener for checkbox

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ checkBoxes.forEach(checkBox=>{
         console.log(this);
     })
 });
-
 const checkBoxesText = document.querySelectorAll('.cursor-point-text');
-checkBoxesText.forEach(checkBoxText =>{
-    checkBoxText.addEventListener('click', function (){
-        this.classList.toggle('active');
-        console.log(this);
-    })
+
+checkBoxesText.forEach(function(checkBoxText) {
+    checkBoxText.addEventListener('click', function() {
+        const checkBox = this.previousElementSibling; // Get the previous sibling, which is the checkbox
+        checkBox.style.backgroundColor = 'black';
+    });
 });
-
-
